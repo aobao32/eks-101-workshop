01be8b7809d4b21adda019bddb6a6807f73e4408
Liu Xinyou <pcman.biti@gmail.com>
2023-06-29 11:56:03 +0800

    Fix ALB ingress config

--- 10/phpdemo-amazonlinux2/src/index.php
+++ 10/phpdemo-amazonlinux2/src/index.php
@@ -1,4 +1,4 @@
 
 <h1>REMOTE_ADDR Address is: <?php printf($_SERVER["REMOTE_ADDR"]); ?></h1>
 
-<h1>X_FORWARD Address is: <?php printf($_SERVER["REMOTE_ADDR"]); ?></h1>
+<h1>X_FORWARD Address is: <?php printf($_SERVER["HTTP_X_FORWARDED_FOR"]); ?></h1>
